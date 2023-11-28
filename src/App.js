df1d67350bdf2113bb693a984ee9fee299b623a3
EliTimeless <EliTimeless@users.noreply.github.com>
2023-11-28 14:24:39 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div>Dictionary</div>
+    </div>
+  );
+}
