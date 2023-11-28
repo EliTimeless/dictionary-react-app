40381e638b8bdee5f633eeb62b22d7734668df6e
EliTimeless <EliTimeless@users.noreply.github.com>
2023-11-28 15:34:49 +0100

    add favicon and search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <div>Dictionary</div>
+      <div className="text-left m-3">Dictionary</div>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">Coded by Eliska Necasova</footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  //musíme zastavit zadanou hodnotu z eventu tak aby se již nehýbala, nastavením setKeyword
+  // spolu s pozicí hodnoty (event.target.value) jinak se nic nepropíše a nelze dál s hodnotou
+  // pracovat. Kdykoliv pak někdo napíše novou hodnotu, useState tuto hodnotu updatuje a pošle
+  // tuto další hodnotu funcki setKeyword.
+  function handleKeywordChange(event) {
+    event.preventDefault(); // zabraňuje dalšímu načítání stránky
+    setKeyword(event.target.value);
+  }
+
+  function Search(event) {
+    event.preventDefault();
+    alert(`Searching ${keyword}`);
+  }
+  ////kdykoliv píšeme do vyhledávače hodnotu, spustí se fce uchovaná v OnChange
+  return (
+    <div className="Dictionary">
+      <form onSubmit={Search}>
+        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
