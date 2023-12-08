754ca557f996e8b2d1b979de1617bb7cb7d8fbd0
EliTimeless <EliTimeless@users.noreply.github.com>
2023-12-08 10:36:01 +0100

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.portrait} className="img-fluid m-1 p-1" />
+                  <img
+                    src={photo.src.portrait}
+                    className="img-fluid m-1 p-1"
+                    alt="image"
+                  />
                 </a>
               </div>
             );
