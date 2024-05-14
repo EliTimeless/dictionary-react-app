c17d85cc34c36a03f70a68f12a37e43869ee23f7
EliTimeless <132577768+EliTimeless@users.noreply.github.com>
2024-05-14 14:41:11 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,12 +6,8 @@ import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
-  let [results, setResults] = useState(null); //nastaví jako výchozí hodnotu "object",
-  //jelikož se hledaná hodnota neustále mění, musíme použít useState, abychom ji mohli zastavit a definovat
-  // pokud chceme předat dál informace, které zachytíme v tomto komponentu a předat je pomocí
-  // proměnné do jiného komponentu, musíme použít useState a pak v novém komponentu použít props.xxx
-  let [loaded, setLoaded] = useState(false); //funkce, ktera se postara o nacitani stranky navic s
-  //pouzitim vyrazu, se kterym se stranka nacte jiz poprve
+  let [results, setResults] = useState(null); 
+  let [loaded, setLoaded] = useState(false); 
   let [photos, setPhotos] = useState(null);
 
   function handleDictionaryResponse(response) {
@@ -31,14 +27,7 @@ export default function Dictionary(props) {
     let scApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${scApiKey}`;
 
     axios.get(scApiUrl).then(handleSCResponse);
-    /*let pexelsApiKey =
-      "YWIj71x3YYRGJ5EzwiLcvtI8YPrdQI3WfwXbbYCffIikUJ9j0zkjw7ey";
-    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
-
-    axios
-      .get(pexelsUrl, { headers: { Authorization: `Bearer ${pexelsApiKey}` } })
-      .then(handlePexelsResponse);
-      */
+   
   }
 
   function handleSubmit(event) {
@@ -46,12 +35,9 @@ export default function Dictionary(props) {
     search();
   }
 
-  //musíme zastavit zadanou hodnotu z eventu tak aby se již nehýbala, nastavením setKeyword
-  // spolu s pozicí hodnoty (event.target.value) jinak se nic nepropíše a nelze dál s hodnotou
-  // pracovat. Kdykoliv pak někdo napíše novou hodnotu, useState tuto hodnotu updatuje a pošle
-  // tuto další hodnotu funcki setKeyword.
+  
   function handleKeywordChange(event) {
-    event.preventDefault(); // zabraňuje dalšímu načítání stránky
+    event.preventDefault(); 
     setKeyword(event.target.value);
   }
 
@@ -61,7 +47,6 @@ export default function Dictionary(props) {
   }
 
   if (loaded) {
-    // pokud se stranka nacte, zobraz toto:
     return (
       <div className="Dictionary">
         <section>
@@ -83,10 +68,8 @@ export default function Dictionary(props) {
       </div>
     );
   } else {
-    // pokud se stranka nenacte, zobraz fci load, ve ktere je fce search, ktera vyhleda vyraz
     load();
     return "loading";
   }
 }
 
-////kdykoliv píšeme do vyhledávače hodnotu, spustí se fce uchovaná v OnChange
